011ecc9799321dbfd2fdca270a3d025e0c0bbddd
Madhav Patel <33404634+madhavpatel249@users.noreply.github.com>
2025-06-10 12:42:34 -0400

    Update solution for reverse-prefix-of-word

--- leetcode/reverse-prefix-of-word.py
+++ leetcode/reverse-prefix-of-word.py
@@ -1,2 +1,22 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        
+        left = 0
+        right = 0
+        string = ""
+
+        for i in range(len(word)):
+            if word[right] == ch:
+                string += word[right::-1]
+                string += word[right+1:]
+                return string
+            else:
+                right += 1
+        
+        
+        return word
